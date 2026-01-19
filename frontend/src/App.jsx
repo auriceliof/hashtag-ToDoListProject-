e9b9b259758e3ec25d73246317a8e0a34fe98eff
auriceliof <auricelio.suporte@hotmail.com>
2026-01-19 16:37:14 -0300

    Initial Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="app-title">Lista de Tarefas</h1>
+
+      {/* Componente - Input Botão */}
+      {/* Componente - Lista de componentes */}
+    </div>
+  );
+}
+
+export default App;
