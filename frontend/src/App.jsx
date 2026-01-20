9aa66242c8f86f4810dae9c4db8a9cef6a7999dc
auriceliof <auricelio.suporte@hotmail.com>
2026-01-20 20:08:53 -0300

    Base Styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import "./index.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
       <h1 className="app-title">Lista de Tarefas</h1>
-
       {/* Componente - Input Botão */}
       {/* Componente - Lista de componentes */}
     </div>
