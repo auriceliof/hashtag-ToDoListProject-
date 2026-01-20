a0d5496eb5c571697579bac41a4e6b6e688024cc
auriceliof <auricelio.suporte@hotmail.com>
2026-01-20 20:15:10 -0300

    Add comppnent AddToDo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import "./App.css";
+import { AddTodo } from "./components/AddTodo";
 
 function App() {
   return (
     <div className="container">
       <h1 className="app-title">Lista de Tarefas</h1>
       {/* Componente - Input Botão */}
+      <AddTodo/>
       {/* Componente - Lista de componentes */}
     </div>
   );
