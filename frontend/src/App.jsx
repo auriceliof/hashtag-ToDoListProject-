d53d00ea090313359d8f841dbcce01eafe7a3198
auriceliof <auricelio.suporte@hotmail.com>
2026-01-21 19:46:51 -0300

    implemented App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import "./index.css";
 import "./App.css";
 import { AddTodo } from "./components/AddTodo";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Exemplo de tarefa", completed: false },
+    { id: 2, text: "Aprender React", completed: false },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  }
+
   return (
     <div className="container">
       <h1 className="app-title">Lista de Tarefas</h1>
       {/* Componente - Input Botão */}
-      <AddTodo onAddTodo={() => {}}/>
+      <AddTodo onAddTodo={addTodo} />
+        {todos.map((todo) => todo.text)}
       {/* Componente - Lista de componentes */}
     </div>
   );
